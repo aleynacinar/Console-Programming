199b07abd223daa0115b5cea726b3a18d6e43e9b
Aleyna <cinar.aleyna@hotmail.com>
2022-04-20 00:24:36 +0300

    works for Patika.dev

---
+++ degisken/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("İsminizi Giriniz");
+string name = Console.ReadLine();
+Console.WriteLine("Soyadınızı Giriniz");
+string lastname = Console.ReadLine();
+Console.WriteLine("Doğum Tarihiniz");
+string bdate = Console.ReadLine();
+Console.WriteLine("Merhaba, " + name + " " + lastname);
